b11f87278d65943f5733557afe345507d5f77518
Zev Hoffman <zevubu@gmail.com>
2019-07-16 23:45:38 -0700

    I made the game steven universe theamed and planned out some special abilities.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,32 @@
 
    $(function(){
     // decided to do the easier version first. The tarot rpg is saved in another git.
+    // initailize
 
     
+
+
+    let numberToHit = Math.floor(Math.random() * 120) + 19;
+
+    console.log(`Number to hit is:${numberToHit}`)
+
+    $("#random-num").text(numberToHit)
+
+    // create random number 19-120
+    // asign random number to crystal gems 1-12
+    // asign random number to backup gems -1 through - 6
+    // limit use of backup to 3 each
+    // asign big guns to apply the exact amount missing to score.
+    // limit big gun to 1 use each.
+    //
+    // on click select random number 
+    // look for win/ loss
+
+
+
+
+    // check for win/ loss
+    // if wins = 5 win game
 
    })
    
