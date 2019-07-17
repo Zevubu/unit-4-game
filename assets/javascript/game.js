130aad2b65ff22dae8a69994012eb757df2d3f7b
Zev Hoffman <zevubu@gmail.com>
2019-07-17 00:08:47 -0700

    added randomizer for characters and edited page text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,45 @@
    $(function(){
     // decided to do the easier version first. The tarot rpg is saved in another git.
     // initailize
+    //  randomly place lion.
+    // randomly select opponent
+
+
+
+
 
     
 
 
-    let numberToHit = Math.floor(Math.random() * 120) + 19;
+    let numberToHit;
+    let characterValueRandom
+    let characterValue=[];
 
-    console.log(`Number to hit is:${numberToHit}`)
+    
 
     $("#random-num").text(numberToHit)
 
-    // create random number 19-120
-    // asign random number to crystal gems 1-12
-    // asign random number to backup gems -1 through - 6
+   //  initailizeGame = function(){
+      numberToHit = Math.floor(Math.random() * 120) + 19;
+      console.log(`Number to hit is:${numberToHit}`);
+      for(let i = 0; i < 4; i++){
+         characterValueRandom = Math.floor(Math.random() *12)+ 1;
+         characterValue.push(characterValueRandom);
+         console.log(` cr ${characterValueRandom}`)
+         
+
+
+      };
+      console.log(`cva${characterValue}`)
+      
+
+
+      // create random number 19-120
+      // asign random number to crystal gems 1-12
+      // asign random number to backup gems -1 through - 6
+   //  }
+
+    
     // limit use of backup to 3 each
     // asign big guns to apply the exact amount missing to score.
     // limit big gun to 1 use each.
